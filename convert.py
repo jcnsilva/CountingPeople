30c3718e4a0b5fc1645295dbfccdbe6748e2a8f2
MatheusAzevedo <matheus.azevedo@ccc.ufcg.edu.br>
2018-03-11 21:21:13 -0300

    Adicionando classe para converter vários frames em um vídeo

---
+++ convert.py
@@ -0,0 +1,17 @@
+import cv2
+import os
+
+image_folder = 'images_sequence/tuk-tuk'
+video_name = 'tuk-tuk.avi'
+
+images = [img for img in os.listdir(image_folder) if img.endswith(".jpg")]
+frame = cv2.imread(os.path.join(image_folder, images[0]))
+height, width, layers = frame.shape
+
+video = cv2.VideoWriter(video_name, -1, 17, (width,height))
+
+for image in images:
+    video.write(cv2.imread(os.path.join(image_folder, image)))
+
+cv2.destroyAllWindows()
+video.release()
